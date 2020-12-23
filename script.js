2ad92088487a5aa9507842e672a654243cd0beff
Davide <davidelucifora1@gmail.com>
2020-12-23 10:58:51 +0530

    Refactor Code.

--- script.js
+++ script.js
@@ -18,53 +18,55 @@ const randomColorButton = document.getElementById('random-button');
 const warmColorButton = document.getElementById('warm-button');
 const coldColorButton = document.getElementById('cold-button');
 const eraseBtn = document.getElementById('erase-btn');
-
 let color = chooseColor()
 
 gridSizeOutput.innerHTML = widthSlider.value;
 
 createGrid(16);
- 
-widthSlider.addEventListener('input', function(){               // Dynamically display grid size when moving slider
+
+ // App Controls Functions:
+
+// Dynamically display grid size choice when moving slider
+ widthSlider.addEventListener('input', function(){               
     let width = widthSlider.value;
     gridSizeOutput.textContent = width;
 });
-
-generateBtn.addEventListener('click', function(){               // Launch createGrid() function when on click
+// Launch createGrid() function when clicking generate grid
+generateBtn.addEventListener('click', function(){               
     width = widthSlider.value;
     createGrid(width);
 });
-
-colorPicker.addEventListener('click', function(){
+// Remove active class from color buttons if using color picker
+colorPicker.addEventListener('click', function(){               
     document.querySelectorAll('.color-btn').forEach(btn => {
         btn.classList.remove('color-btn-active');
     })
 })
-
+// Random color button
 randomColorButton.addEventListener('click', function(e){
     warmColorButton.classList.remove('color-btn-active')
     coldColorButton.classList.remove('color-btn-active')
     e.target.classList.toggle('color-btn-active');
-    
 });
+// Warm color button
 warmColorButton.addEventListener('click', function(e){
     randomColorButton.classList.remove('color-btn-active')
     coldColorButton.classList.remove('color-btn-active')
     e.target.classList.toggle('color-btn-active');
-    
 });
+// Cold color button
 coldColorButton.addEventListener('click', function(e){
     warmColorButton.classList.remove('color-btn-active')
     randomColorButton.classList.remove('color-btn-active')
     e.target.classList.toggle('color-btn-active');
-    
 });
-
+// Erase grid button
 eraseBtn.addEventListener('click', function(e){
     document.querySelectorAll('.grid-cell').forEach(cell => {
         cell.style.backgroundColor = 'white';
     })
 });
+
 
 function createGrid(width){
     // Clean Up previous Grid
@@ -85,11 +87,9 @@ function createGrid(width){
         cell.addEventListener('mouseover', function(){
             this.style.backgroundColor = chooseColor();
         })
-        cell.addEventListener('mouseout', function(){
-        })
         });
 }
-
+//Assign value of variable color based on choice
 function chooseColor(){
     if (randomColorButton.classList.contains('color-btn-active')){
         return `rgb(${randomRange(0,255)},${randomRange(0,255)},${randomRange(0,255)})`
@@ -104,7 +104,7 @@ function chooseColor(){
         return colorPicker.value
     }
 }
-
+// Generate random range for RGB colors
 function randomRange(min, max){
     return Math.floor(Math.random()*(max - min) + min)
 }
